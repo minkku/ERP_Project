cf58a6f331226855172d54f8d86f01dd5731f6b5
minkyu <0106995@naver.com>
2024-01-10 15:52:09 +0900

    JWT later..

--- src/main/java/org/gagu/config/SecurityConfig.java
+++ src/main/java/org/gagu/config/SecurityConfig.java
@@ -7,40 +7,44 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-
 
 @Configuration
 @EnableWebSecurity
 @Log4j2
 public class SecurityConfig {
+    private final String[] allowedUrls = {"/", "/login"};
+    private final String[] needAuth = {"/main"};
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-
-//    @Bean
-//    public InMemoryUserDetailsManager userDetailsManager() {
-//        UserDetails user = User.builder()
-//
-//    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         log.info("------------------- filterChain -------------------");
         return http
                 .csrf(AbstractHttpConfigurer::disable)
+
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/", "/login").permitAll()
-                        .requestMatchers("/main").authenticated()
+                        .requestMatchers(allowedUrls).permitAll()
+                        .requestMatchers(needAuth).authenticated()
                         .anyRequest().authenticated()
                 )
+                // session and cookies X cuz using JWT
+//                .sessionManagement(sessionManagement -> sessionManagement
+//                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+//                )
+
                 .formLogin(formLogin -> formLogin
                         .defaultSuccessUrl("/main", true)
                         .successForwardUrl("/main")
                 )
+
                 .logout(Customizer.withDefaults())
                 .build();
     }
---
+++ src/main/java/org/gagu/repository/member/AuthServiceImpl.java
@@ -0,0 +1,21 @@
+//package org.gagu.repository.member;
+//
+//import org.gagu.entity.member.Member;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//public class AuthServiceImpl implements AuthService {
+//    @Autowired
+//    private MemberRepository memberRepository;
+//
+//    @Override
+//    public Member loginUser(String memberId, String password) {
+//        Member member = memberRepository.findByUserId(memberId);
+//
+//        if(member == null) throws new Exception("Can not find User");
+//
+//
+//    }
+//}
+//// 보류
