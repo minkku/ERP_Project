2cc80e8a1337d6b87e3a29944f16ad4ddd7d588e
minkyu <0106995@naver.com>
2024-01-17 17:45:49 +0900

    Add query for update, still working on it

--- src/main/java/org/gagu/repository/businessPartner/PartnerRepository.java
+++ src/main/java/org/gagu/repository/businessPartner/PartnerRepository.java
@@ -4,16 +4,35 @@ import org.gagu.dto.business.BusinessPartnerDetailResponse;
 import org.gagu.dto.business.BusinessPartnerResponse;
 import org.gagu.entity.businessPartner.BusinessPartner;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface PartnerRepository extends JpaRepository<BusinessPartner, Integer>, PartnerCustomRepository {
     @Query("SELECT NEW org.gagu.dto.business.BusinessPartnerResponse(" +
-            "b.partnerId, " +
-            "b.bpName, " +
-            "b.companyNumber, " +
-            "b.phone, " +
-            "b.address) " +
+                "b.partnerId, " +
+                "b.bpName, " +
+                "b.companyNumber, " +
+                "b.phone, " +
+                "b.address) " +
             "FROM BusinessPartner b ")
     BusinessPartnerResponse findAll(@Param("partnerId") int partnerId);
+
+    @Modifying(clearAutomatically = true)
+    @Query("UPDATE " +
+                "businesspartner bp " +
+            "SET " +
+                "bp.bpName = :bpName, " +
+                "bp.companyNumber = :companyNumber, " +
+                "bp.phone = :phone, " +
+                "bp.fax = :fax, " +
+                "bp.address = :address, " +
+                "bp.headName = :headName, " +
+                "bp.type = :type, " +
+                "bp.note = :note, " +
+                "bp.bigType = :bigType, " +
+                "bp.smallType = :smallType " +
+            "WHERE " +
+                "bp.partnerId = :partnerId")
+    int updateByPartnerId(@Param("partnerId") int partnerId);
 }
